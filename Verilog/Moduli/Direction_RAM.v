651ded14f5010f013bcfb7d2b53d1c469127febb
MonicaManai <m.manai1@studenti.uniss.it>
2025-01-03 18:29:11 +0100

    Alcuni accorgimenti

--- Verilog/Moduli/Direction_RAM.v
+++ Verilog/Moduli/Direction_RAM.v
@@ -4,19 +4,23 @@ module Direction_RAM #(
 ) (
     input wire clk,rst,
     input wire en_ins,we,en_traceB,
-    input wire [BitAddr:0] i, j, i_t, j_t,
+    input wire [BitAddr:0] i_in, j_in, i_t, j_t,
     input wire [2:0] symbol_in,
     output reg [2:0] symbol_out
 );
 
     reg [2:0] ram [N*N:0]; /*the ram needs to be 129*129 because we need 128 characters +1 of gap for both the strings
                             then each cell needs to be of 3 bits because we need them for the arrow < > \*/
+    
+    wire [BitAddr:0] i,j; 
+    assign i=i_in+1; // because in the "matrix" we have a gap cell before the sequence
+    assign j=j_in+1; // ''
 
     /*i think we need to add the initializzation even to ths ram for all the arrows in the first column and in tha first row
     that points to the 0*/
     always @(posedge clk, posedge rst/*,en_init,en_read,en_ins,we*/) begin
         if(rst) begin
-            //something
+            symbol_out<=0;
         end
         else if(en_ins) begin
             if(we)
--- Verilog/Moduli/Scores_RAM.v
+++ Verilog/Moduli/Scores_RAM.v
@@ -4,7 +4,7 @@ module Scores_RAM #(
 ) (
     input wire clk,rst,
     input wire en_init,en_ins_read,we,
-    input wire [BitAddr:0] addr, i, j,
+    input wire [BitAddr:0] addr, i_in, j_in,
     input wire [8:0] max, data,
     output reg [8:0] diag, up, left
 );
@@ -12,9 +12,15 @@ module Scores_RAM #(
     reg [8:0] ram [N*N:0]; /*the ram needs to be 129*129 because we need 128 characters +1 of gap for both the strings
                             then each cell needs to be of 9 bits because that's how many bits are needed to store the numbers -128 to +128 in c2*/
 
+    wire [BitAddr:0] i,j; 
+    assign i=i_in+1; // because in the "matrix" we have a gap cell before the sequence
+    assign j=j_in+1; // ''
+
     always @(posedge clk, posedge rst/*,en_init,en_read,en_ins,we*/) begin
         if(rst) begin
-            //something
+            diag<=0;
+            up<=0;
+            left<=0;
         end
         else begin
             if(en_init) begin
