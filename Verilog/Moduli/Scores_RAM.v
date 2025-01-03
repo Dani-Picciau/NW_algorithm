61f9ea342602f663deed3a5560098c5b4d2302e9
MonicaManai <m.manai1@studenti.uniss.it>
2025-01-04 00:38:50 +0100

    altre cose

--- Verilog/Moduli/Scores_RAM.v
+++ Verilog/Moduli/Scores_RAM.v
@@ -3,17 +3,13 @@ module Scores_RAM #(
 ) (
     input wire clk,rst,
     input wire en_init,en_ins_read,we,
-    input wire [BitAddr:0] addr, i_in, j_in,
+    input wire [BitAddr:0] addr, i, j,
     input wire [8:0] max, data,
     output reg [8:0] diag, up, left
 );
     parameter BitAddr = $clog2(N);
     reg [8:0] ram [N*N:0]; /*the ram needs to be 129*129 because we need 128 characters +1 of gap for both the strings
     then each cell needs to be of 9 bits because that's how many bits are needed to store the numbers -128 to +128 in c2*/
-
-    wire [BitAddr:0] i,j; 
-    assign i=i_in+1; // because in the "matrix" we have a gap cell before the sequence
-    assign j=j_in+1; // ''
 
     always @(posedge clk, posedge rst/*,en_init,en_read,en_ins,we*/) begin
         if(rst) begin
@@ -29,11 +25,11 @@ module Scores_RAM #(
                 end
             end
             else if(en_ins_read) begin
-                if(we) ram[i+N*j] <= max;  
+                if(we) ram[(i+1)+(N*(j+1))] <= max;  
                 else begin
-                    diag<=ram[(i-1)+N*(j-1)];
-                    up<=ram[(i-1)+N*j];
-                    left<=ram[i+N*(j-1)];
+                    diag<=ram[i+N*j];
+                    left<=ram[(i+1)+N*j];
+                    up<=ram[i+N*(j+1)];
                 end
             end
         end
