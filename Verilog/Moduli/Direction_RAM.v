6be8a7ece4f5eaf09ae89f8d408305301fde8a93
MonicaManai <m.manai1@studenti.uniss.it>
2025-01-04 16:59:11 +0100

    Alcuni accorgimenti

--- Verilog/Moduli/Direction_RAM.v
+++ Verilog/Moduli/Direction_RAM.v
@@ -1,6 +1,6 @@
 module Direction_RAM #(
     parameter N=128, 
-    parameter BitAddr = $clog2(N)
+    parameter BitAddr = $clog2(N+1)
 ) (
     input wire clk,rst,
     input wire en_ins, we, en_traceB, en_init,
@@ -9,7 +9,7 @@ module Direction_RAM #(
     output reg [2:0] symbol_out, simbolo
 );
 
-    reg [2:0] ram [N*N:0]; /*the ram needs to be 129*129 because we need 128 characters +1 of gap for both the strings then each cell needs to be of 3 bits because we need them for the arrow: <-, ⭡, ↖ */
+    reg [2:0] ram [((N+1)*(N+1))-1:0]; /*the ram needs to be 129*129 because we need 128 characters +1 of gap for both the strings then each cell needs to be of 3 bits because we need them for the arrow: <-, ⭡, ↖ */
     
     parameter UP=3'b010, LEFT=3'b100;
     
@@ -20,9 +20,9 @@ module Direction_RAM #(
         else if (en_init && we) begin
             /*initializzation for all the arrows, in the first column and in the first row, that points to the 0*/
             ram[addr] <= LEFT;
-            ram[N*addr] <= UP; 
+            ram[(N+1)*addr] <= UP; 
         end
-        else if(en_ins && we) ram[(i+1)+(N*(j+1))] <= symbol_in;
-        else if(en_traceB) symbol_out <= ram[i_t + N * j_t];
+        else if(en_ins && we) ram[(i+1)+((N+1)*(j+1))] <= symbol_in;
+        else if(en_traceB) symbol_out <= ram[i_t + (N+1) * j_t];
     end
 endmodule
