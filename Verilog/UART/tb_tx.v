b314e20d1f9d4b1aa65feceb8b69b62910973b17
matteopedoni25 <matteopedoni25@gmail.com>
2025-01-08 17:19:17 +0100

    updates

--- Verilog/UART/tb_tx.v
+++ Verilog/UART/tb_tx.v
@@ -1,3 +1,4 @@
+`include "C:\Users\crist\Desktop\GitHUb\Progetto ESD\NW_algorithm\Verilog\UART"
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: University of Sassari
@@ -8,24 +9,24 @@
 
 
 module tb_tx;
-	parameter DATA_BW=8, DATA_BW_BIT=4, 
-			  BAUD_RATE=9600,
-			  BAUD_COUNT=10416, //with BAUD_RATE 9600bps and system clk 100MHz --> baud rate counter is 10416 represented with N_BIT=14
-			  BAUD_BIT=14,
-			  OVER_SAMPL=16, 
-			  OVER_SAMPL_BIT=5,
-			  BAUD_COUNT_x16=651, //with BAUD_RATE 9600bps, oversampling 16 and system clk 100MHz --> baud rate counter x 16 is 651 represented with 10 bits
-			  BAUD_BIT_x16=10;
+	parameter 
+		DATA_BW=8, 
+		DATA_BW_BIT=4, 
+		BAUD_RATE=9600,
+		BAUD_COUNT=10416, //with BAUD_RATE 9600bps and system clk 100MHz --> baud rate counter is 10416 represented with N_BIT=14
+		BAUD_BIT=14;
 			  
 	reg clk, rst;
 	reg transmit;
 	reg [DATA_BW-1:0] input_data;
-	wire [DATA_BW-1:0] data_out;
-	wire busy, ready;
+	wire TX; 
+	wire busy;
 				   
-	top_DUT #(.DATA_BW(DATA_BW), .DATA_BW_BIT(DATA_BW_BIT), .BAUD_RATE(BAUD_RATE), .BAUD_COUNT(BAUD_COUNT), .BAUD_BIT(BAUD_BIT),
-				.BAUD_COUNT_x16(BAUD_COUNT_x16), .BAUD_BIT_x16(BAUD_BIT_x16), .OVER_SAMPL(OVER_SAMPL), .OVER_SAMPL_BIT(OVER_SAMPL_BIT)
-			) DUT(.clk(clk), .rst(rst), .transmit(transmit), .data_in(input_data), .busy(busy), .data_out(data_out), .ready(ready));
+	top_tx #(.DATA_BW(DATA_BW), .DATA_BW_BIT(DATA_BW_BIT),
+				.BAUD_RATE(BAUD_RATE), .BAUD_COUNT(BAUD_COUNT),
+	 			.BAUD_BIT(BAUD_BIT))
+			DUT(.clk(clk), .rst(rst), .transmit(transmit),
+			 .data_in(input_data),.TX(TX), .busy(busy))	
 
 	//generazione clock simulazione
 	always #5 clk=~clk;
@@ -59,7 +60,7 @@ module tb_tx;
 				@(negedge clk)
 				transmit <= 1'b0; // il transmit rimane alto un colpo di clock 
 				/*non si esce dal task (quindi non si abilitano potenzialmente nuove transazioni) 
-				fino a che la UART non è di nuovo disponibile*/
+				fino a che la UART non ï¿½ di nuovo disponibile*/
 				@(negedge busy); 
 			end
 	endtask
