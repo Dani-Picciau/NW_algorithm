836a93685294b596ec298b79a7500c2985d3b34e
Dani-Picciau <dany26621@gmail.com>
2025-01-10 22:16:52 +0100

    some updates

--- Verilog/Modules/Score_RAM_management/Counter_3.v
+++ Verilog/Modules/Score_RAM_management/Counter_3.v
@@ -1,12 +1,19 @@
 module Counter_3 (
     input wire clk, rst,
     input wire en,
-    input wire start_count,
     output reg signal,
     output reg [1:0] count
 );
     reg [1:0] count_next;
+    reg [1:0] cnt_clk, cnt_clk_nxt;
 
+    //Register to count the clock
+    always @(posedge clk, posedge rst) begin
+        if(rst) cnt_clk <= 0;
+        else cnt_clk <= cnt_clk_nxt;
+    end
+
+    //Register to increase the count
     always @(posedge clk, posedge rst) begin
         if(rst) begin
             count <= 0;
@@ -21,8 +28,16 @@ module Counter_3 (
                 signal = 1'b1;
                 count_next <= 0;
             end 
-            else if(start_count) count_next <= count + 1;
-            else count_next <= count;
+            else begin
+                if(cnt_clk == 2'b10) begin
+                    count_next <= count + 1;
+                    cnt_clk_nxt <= 0;
+                end
+                else begin
+                    cnt_clk_nxt <= cnt_clk + 1;
+                    count_next <= count;
+                end
+            end
         end
     end
 endmodule
--- Verilog/Modules/Score_RAM_management/Score_manager.v
+++ Verilog/Modules/Score_RAM_management/Score_manager.v
@@ -14,10 +14,8 @@ module Score_manager #(
     input wire [8:0] data_in,
     input wire [8:0] max,
     input wire [BitAddr:0] i, j,
-    input wire start_count_3,
     input wire en_counter_3,
 
-    output reg stop_count_3,
     output reg signal,
     output reg [8:0] diag, left, up
 );
@@ -28,7 +26,7 @@ module Score_manager #(
     assign en_din = (en_init | en_ins);
 
     Scores_RAM #(.N(N)) S_RAM(.clk(clk), .rst(rst), .din(data), .en_din(en_din), .en_dout(en_read), .we(we), .addr_din(addr_w), .addr_dout(addr_r), .dout(score));
-    Counter_3 C_3 (.clk(clk), .rst(rst), .en(en_counter_3), .start_count(start_count_3), .signal(signal), .count(count_3));
+    Counter_3 C_3 (.clk(clk), .rst(rst), .en(en_counter_3), .signal(signal), .count(count_3));
     Counter_1 C_1 (.clk(clk), .rst(rst), .en_init(en_init), .hit(hit));
     Writing_index_score #(.N(N)) W_i_s (.clk(clk), .rst(rst), .en_ins(en_ins), .en_init(en_init), .hit(hit), .i(i), .j(j), .addr_init(addr), .max(max), .data_init(data_in), .addr_out(addr_w), .data_out(data));
     Reading_index_score #(.N(N)) R_i_s (.clk(clk), .rst(rst), .en_read(en_read), .signal(count_3), .i(i), .j(j), .addr(addr_r));
