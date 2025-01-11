42f9bb679ed080230f8f5f8a35b66fdb9a788878
MonicaManai <m.manai1@studenti.uniss.it>
2025-01-11 10:16:55 +0100

    TB score ram

--- Verilog/Modules/Score_RAM_management/Scores_RAM.v
+++ Verilog/Modules/Score_RAM_management/Scores_RAM.v
@@ -3,10 +3,12 @@ module Scores_RAM #(
     parameter BitAddr = $clog2(N+1)
 ) (
     input wire clk, rst,
-    input wire [8:0] din,
-    input wire en_din, en_dout, we, //en_din is an OR between en_ins and en_init
+    input wire [8:0] din, // data in, to be written
+    input wire en_din, //en_din (en_write) is an OR between en_ins and en_init
+    input wire en_dout, //en_read
+    input wire we, 
     input wire [BitAddr:0] addr_din, addr_dout,
-    output reg [8:0] dout
+    output reg [8:0] dout //data out, to be read
 );
     reg [8:0] ram [((N+1)*(N+1))-1:0]; /*The ram needs to be 129*129 because we need 128 characters +1 of gap for both the strings then each cell needs to be of 9 bits because that's how many bits are needed to store the numbers -128 to +128 in c2*/
 
---
+++ Verilog/TB_Modules/TB_Score_RAM_managemet/TB_Scores_RAM.v
@@ -0,0 +1,40 @@
+`include "/c:.../Score_RAM.v"
+
+module TB_Score_RAM;
+    
+    //parameter
+    parameter N=128; 
+    parameter BitAddr = $clog2(N+1);
+    //inputs
+    reg clk, rst;
+    reg [8:0] din;
+    reg en_din, en_dout, we; //en_din is an OR between en_ins and en_init
+    reg [BitAddr:0] addr_din; 
+    reg [BitAddr:0] addr_dout,
+    //output
+    wire [8:0] dout;
+
+    always #1 clk = ~clk;
+
+    initial begin
+        clk = 0; rst = 1; 
+        din = 9'b0; 
+        en_din = 0; en_dout = 0; we = 0;
+        addr_din = 0; 
+        addr_dout = 0; 
+
+        #5 rst = 0;
+        en_din =1 ; we = 1;
+        addr_din = 0; din = 1;
+        #5
+        addr_din = 1; din = 5;
+        #5
+        en_din = 0; we = 0;
+        en_dout = 1;
+        addr_dout = 0;
+        #5
+        addr_dout = 1;
+    end
+
+    //commento
+endmodule
