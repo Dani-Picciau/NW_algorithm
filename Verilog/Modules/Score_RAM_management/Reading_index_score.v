2ff63447b467944f999a3b0171aa5d3c88d5708d
AndreaFiori03 <a.fiori100@studenti.uniss.it>
2025-01-15 15:52:21 +0100

    Update Reading_index_score.v

--- Verilog/Modules/Score_RAM_management/Reading_index_score.v
+++ Verilog/Modules/Score_RAM_management/Reading_index_score.v
@@ -7,27 +7,26 @@ module Reading_index_score #(
     input wire en_read,
     input wire [1:0] count,
     input wire [BitAddr:0] i, j,
-    output reg [addr_lenght:0] addr,
-    output reg valid
+    output reg [addr_lenght:0] addr
 );
     
     always @(posedge clk, posedge rst) begin
         case ({en_read, count})
             3'b100: begin
                 addr <= (j+(N+1)*i); // Index for the diagonal
-                valid <= 1;
+         
             end 
             3'b101: begin
                 addr <= ((j+1)+(N+1)*i); // Index for the up
-                valid <= 1;
+           
             end
             3'b110: begin
                 addr <= (j+(N+1)*(i+1)); // Index for the left
-                valid <= 1;
+               
             end 
             default: begin
                 addr <= {addr_lenght{1'bx}};
-                valid <= 0;
+        
             end
         endcase
     end
