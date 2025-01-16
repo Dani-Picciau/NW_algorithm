730bc130e9c5983a1d6d5c066064c9a7ac88086a
Dani-Picciau <dany26621@gmail.com>
2025-01-16 23:12:28 +0100

    lastest updates

--- Verilog/Modules/Signal_management_for_the_RAM/Signal_manager.v
+++ Verilog/Modules/Signal_management_for_the_RAM/Signal_manager.v
@@ -5,8 +5,24 @@
 
 module Signal_manager #(
     parameter N = 128
-) (
-
+)(
+    input wire clk, rst,
+    input wire en_read,
+    input wire change_index,      
+    output wire end_filling,
+    output wire [($clog2(N)):0] i, j
 );
-    
+
+    Insertion_counter #(
+        .N(N)
+    ) I_c (
+        .clk(clk),
+        .rst(rst),
+        .en_read(en_read),
+        .change_index(change_index),
+        .end_filling(end_filling),
+        .i(i),
+        .j(j)
+    );
+
 endmodule
