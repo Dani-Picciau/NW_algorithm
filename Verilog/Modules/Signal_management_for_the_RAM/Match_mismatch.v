c6721afa2b369ad4a41cd7ec3ce2efa08903669c
Dani-Picciau <dany26621@gmail.com>
2025-02-06 13:52:59 +0100

    some updates

--- Verilog/Modules/Signal_management_for_the_RAM/Match_mismatch.v
+++ Verilog/Modules/Signal_management_for_the_RAM/Match_mismatch.v
@@ -1,11 +1,11 @@
-module match_mismatch(
+module Match_mismatch(
+    input wire clk,
     input wire [2:0] a,     
     input wire [2:0] b,     
     output reg  value
 );
-    always @(a,b) begin
+    always @(posedge clk, a, b) begin
         if (a == b) value = 1'b1; 
         else value = 1'b0;  
     end
-    
 endmodule
