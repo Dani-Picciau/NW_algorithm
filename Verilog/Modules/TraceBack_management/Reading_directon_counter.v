465c710429615c1a712e386d699c2da9c04b8651
Dani-Picciau <dany26621@gmail.com>
2025-02-11 17:01:33 +0100

    .

--- Verilog/Modules/TraceBack_management/Reading_directon_counter.v
+++ Verilog/Modules/TraceBack_management/Reading_directon_counter.v
@@ -6,57 +6,57 @@ module RD_Counter#(
     input wire en_traceB,
     input wire [2:0] symbol,
     output reg end_c,
-    output reg [BitAddr:0] i, j
+    output reg [BitAddr:0] i_t, j_t
 );
     reg [BitAddr:0] i_nxt, j_nxt;
     parameter [2:0] UP=3'b010, LEFT=3'b100, DIAG=3'b001;
 
     always @(posedge clk, posedge rst) begin
         if(rst) begin
-            i <= N+1;
-            j <= N+1;
+            i_t <= N+1;
+            j_t <= N+1;
         end
         else begin
-            i <= i_nxt;
-            j <= j_nxt;
+            i_t <= i_nxt;
+            j_t <= j_nxt;
         end
     end
 
     always @(posedge clk) begin
-        if(i!=0 && j!=0)begin
+        if(i_t!=0 && j_t!=0)begin
             end_c=0;
             case (symbol)
                 UP: begin
-                    i_nxt <= i-1;
-                    j_nxt <= j;
+                    i_nxt <= i_t-1;
+                    j_nxt <= j_t;
                 end
                 LEFT: begin
-                    i_nxt <= i;
-                    j_nxt <= j-1;
+                    i_nxt <= i_t;
+                    j_nxt <= j_t-1;
                 end
                 DIAG: begin
-                    i_nxt <= i-1;
-                    j_nxt <= j-1;
+                    i_nxt <= i_t-1;
+                    j_nxt <= j_t-1;
                 end
                 default: begin
-                    i_nxt <= i;
-                    j_nxt <= j;
+                    i_nxt <= i_t;
+                    j_nxt <= j_t;
                 end
             endcase
         end
         else begin
-            if(i==0 && j==0) end_c=1;
+            if(i_t==0 && j_t==0) end_c=1;
             else begin
                 end_c=0;
-                if(i==0 && j!=0 && symbol==LEFT) begin
-                    i_nxt <= i;
-                    j_nxt <= j-1;
-                end else if(i!=0 && j==0 && symbol==UP) begin
-                    i_nxt <= i-1;
-                    j_nxt <= j;
+                if(i_t==0 && j_t!=0 && symbol==LEFT) begin
+                    i_nxt <= i_t;
+                    j_nxt <= j_t-1;
+                end else if(i_t!=0 && j_t==0 && symbol==UP) begin
+                    i_nxt <= i_t-1;
+                    j_nxt <= j_t;
                 end else begin
-                    i_nxt <= i;
-                    j_nxt <= j;
+                    i_nxt <= i_t;
+                    j_nxt <= j_t;
                 end
            end
         end
