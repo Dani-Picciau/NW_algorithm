f3c3703b8eeab1210c7d5f22132576932e70a693
MonicaManai <m.manai1@studenti.uniss.it>
2025-02-12 15:41:31 +0100

    RAM di prova

--- Verilog/Modules/RAM_B_di_prova.v
+++ Verilog/Modules/RAM_B_di_prova.v
@@ -5,29 +5,29 @@ module RAM_B #(
     parameter Bit = $clog2(N)
 ) (
     input wire clk, rst,
-    input wire [8:0] din, // data in, to be written
-    input wire en_din, 
+    //input wire [8:0] din, // data in, to be written
+    //input wire en_din, 
     input wire en_dout, //en_read || en_trace_B
-    input wire we, 
-    input wire [Bit-1:0] addr_din, addr_dout,
+    //input wire we, 
+    input wire [Bit-1:0] /*addr_din,*/ addr_dout,
     output reg [8:0] dout //data out, to be read
 );
     reg [7:0] ram [N-1:0]; //128 cells, from 127 to 0. Every cell is 8 bits, from 7 to 0, for the ascii
 
     initial 
     begin
-        ram[0]=8'h43; // C
-        ram[1]=8'h54; // T
-        ram[2]=8'h47; // G
-        ram[3]=8'h41; // A
-        ram[4]=8'h54; // T
+        ram[0]=8'h47; // G
+        ram[1]=8'h41; // A
+        ram[2]=8'h54; // T
+        ram[3]=8'h47; // G
+        ram[4]=8'h43; // C
     end
 
-    always @(posedge clk) begin
-        if(en_din)begin
-            if(we) ram[addr_din] <= din;
-        end
-    end
+    // always @(posedge clk) begin
+    //     if(en_din)begin
+    //         if(we) ram[addr_din] <= din;
+    //     end
+    // end
 
     always @(posedge clk, posedge rst) begin
         if(rst) dout <= 0;
