687eaa9743766cfd3e6ae917bc4fd862046de787
AndreaFiori03 <a.fiori100@studenti.uniss.it>
2025-02-20 13:02:00 +0100

    Update TB_Output_manager.v

--- Verilog/TB_Modules/TB_Score_RAM_management/TB_Output_manager.v
+++ Verilog/TB_Modules/TB_Score_RAM_management/TB_Output_manager.v
@@ -2,9 +2,10 @@
 
 module TB_Output_manager;
 
-    // Signal inputs
+   // Signal inputs
     reg clk, rst;
     reg en_read;
+    reg signal; 
     reg [1:0] count;
     reg [8:0] ram_data;
 
@@ -17,8 +18,53 @@ module TB_Output_manager;
         .rst(rst), 
         .en_read(en_read), 
         .ram_data(ram_data), 
+        .signal(signal),
         .diag(diag), 
         .left(left),    
+        .up(up), 
+        .count(count)
+    );
+
+    // Clock generation
+    always #0.5 clk = ~clk;
+    initial begin
+
+        // Initializzation of signals
+        clk = 0; rst = 1; en_read = 0; ram_data = 0;signal=0;count=0;
+
+        // Sending signals ram_data
+        #5.5  rst = 0; en_read = 1; 
+            count = 0;
+            ram_data = 9;
+        #6  count = 1;
+            ram_data = 8;
+        #6  count = 2;
+            ram_data = 7;
+        #5  signal = 1;
+        #1  signal=0;
+            
+
+            count = 0;
+            ram_data = 6;
+        #6  count = 1;
+            ram_data = 5; 
+        #6  count = 2;
+            ram_data = 4;
+        #5  signal = 1;
+        #1  signal=0;
+
+            count = 0;
+            ram_data = 3;
+        #6  count = 1;
+            ram_data = 2;
+        #6  count = 2;
+            ram_data = 1; 
+        #5  signal = 1;
+        #1  signal=0;
+
+        #4 $stop;
+    end
+endmodule
         .up(up), 
         .count(count)
     );
