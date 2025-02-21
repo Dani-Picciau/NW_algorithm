f9721dfcce7e2d3be9d81c620b6f211b79abaa5d
AndreaFiori03 <a.fiori100@studenti.uniss.it>
2025-02-21 11:03:34 +0100

    Update TB_Counter_1_dir.v

---
+++ Verilog/TB_Modules/TB_Direction_RAM_management/TB_Counter_1_dir.v
@@ -0,0 +1,32 @@
+module TB;
+
+    // Signal inputs
+    reg clk, rst;
+    reg en_init;
+
+    // Signal outpus
+    wire hit;
+
+    // Instantiation of the Counter_1 module
+    Counter_1_sc test (
+        .clk(clk),
+        .rst(rst),
+        .en_init(en_init),
+        .hit(hit)
+    );
+
+    // Clock generation
+    always #0.5 clk = ~clk;
+
+    initial begin
+        // Initializzation of signals
+        clk = 0; rst = 1; en_init = 0;
+
+        // Start counting
+        #5 rst = 0;
+        #5 en_init = 1;
+        
+        #10
+        $stop;
+    end
+endmodule
