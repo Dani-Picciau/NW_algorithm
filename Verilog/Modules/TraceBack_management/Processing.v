7baaa817de1813223eab35059f036c2c3814958a
AndreaFiori03 <a.fiori100@studenti.uniss.it>
2025-02-22 17:08:32 +0100

    Update Processing.v

--- Verilog/Modules/TraceBack_management/Processing.v
+++ Verilog/Modules/TraceBack_management/Processing.v
@@ -1,4 +1,3 @@
-
 module Processing #(
     parameter N = 128,
     parameter score_length = $clog2(N+1),
@@ -16,24 +15,24 @@ module Processing #(
     output reg [2:0] datoA,
     output reg [2:0] datoB
 );
-    reg signed [score_length:0] score_next;
+    reg signed [score_length:0] score_next, score_next_comb;
 
-    //Register for the output score...
+    // Register for the output score
     always @(posedge clk, posedge rst) begin
-        if(rst) final_score <= 0;
+        if (rst) final_score <= 0;
         else final_score <= score_next;
     end
 
     always @(posedge clk, posedge rst) begin
-        if(rst) begin
+        if (rst) begin
             datoA <= 0;
             datoB <= 0;
-            score_next = 0;
+            score_next <= 0;
         end 
         else begin
-            if(en_traceB) begin
+            if (en_traceB) begin
                 case (symbol)
-                    3'b001: begin // Diagonal arrow \
+                    3'b001: begin // Diagonal arrow ?
                         datoA <= SeqA_i_t;
                         datoB <= SeqB_j_t;
                     end
@@ -55,23 +54,26 @@ module Processing #(
                 datoA <= 0;
                 datoB <= 0;
             end
+
+            // synchronized update of the score
+            score_next <= score_next_comb;
         end
     end
 
-    always @(posedge rst, datoA, datoB) begin
-    
-            if(en_traceB) begin
-                case (symbol)
-                    3'b001: begin // Diagonal arrow ?
-                        if(datoA == datoB) score_next = final_score + match_score;
-                        else score_next = final_score + mismatch_score;
-                    end
-                    3'b100: score_next = final_score + gap_score; // Left arrow <-
-                    3'b010: score_next = final_score + gap_score; // Up arrow ^
-                    default: score_next = final_score;
-                endcase
-            end
-            else score_next = 0;
+    // Logica combinatoria per il calcolo del punteggio
+    always @(datoA, datoB) begin
+        if (en_traceB) begin
+            case (symbol)
+                3'b001: begin // Diagonal arrow ?
+                    if (datoA == datoB) score_next_comb = final_score + match_score;
+                    else score_next_comb = final_score + mismatch_score;
+                end
+                3'b100: score_next_comb = final_score + gap_score; // Left arrow <-
+                3'b010: score_next_comb = final_score + gap_score; // Up arrow ^
+                default: score_next_comb = final_score;
+            endcase
+        end
+        else score_next_comb = 0;
     end
     
     //end
